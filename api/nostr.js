68c2e5b011b2d66d872f4adfedce9f0a80748460
Efraim Bart <efraimbart@gmail.com>
2023-06-27 16:51:22 -0400

    Switched over to the Everything SDK and misc fixes

--- api/nostr.js
+++ api/nostr.js
@@ -2,6 +2,7 @@
 import ogs from 'open-graph-scraper'
 import probeImageSize from 'probe-image-size'
 import {nip05, nip19, SimplePool} from "nostr-tools";
+import {Everything} from "everything-sdk";
 
 const urlRegex = /https?:\/\/(?:www\.)?([-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6})\b[-a-zA-Z0-9()@:%_+.~#?&\/=]*/gm
 const onlyUrlRegex = new RegExp(`^${urlRegex.source}$`)
@@ -20,7 +21,7 @@ async function getEvents({limit, filter = {}}) {
         // "wss://nostr-pub.wellorder.net",
         // "wss://nostr.mom",
         // "wss://no.str.cr",
-        // "wss://nostr.wine",
+        "wss://nostr.wine",
         // "wss://e.nos.lol",
         // "wss://relay.nostrdocs.com",
         // "wss://nostr-pub1.southflorida.ninja",
@@ -33,7 +34,7 @@ async function getEvents({limit, filter = {}}) {
         // "wss://nostr-relay.derekross.me",
         // "wss://relay.nostr.bg",
         // "wss://nostr.bitcoiner.social",
-        // "wss://nos.lol",
+        "wss://nos.lol",
         // "wss://relay.nostr.ch",
         // "wss://relay.mostr.pub",
         // "wss://atlas.nostr.land",
@@ -350,7 +351,7 @@ export const displayNameOrUsername = (user, pubkey) => displayName(user) || user
 export const displayName = (user) => user.display_name || user.displayName || user.name || user.username
 export const username = (user, pubkey) => user.nip05 || nip19.npubEncode(pubkey)
 export const author = (user, pubkey)=> user ? displayName(user) ? `${displayName(user)} (${username(user, pubkey)})` : username(user, pubkey) : nip19.npubEncode(pubkey);
-export const subject = event => event.tags.find(tag => tag[0] === 'subject')?.[1]
+export const subject = event => event.tags.find(tag => tag[0] === 'subject')?.[1] || ''
 export const isNotReply = event => !isReply(event)
 //TODO: Consider if there is no marker
 export const processContent = async event => {
@@ -400,175 +401,59 @@ export const isReply = event => event.tags
     .some(tag => tag[3] !== "mention")
 
 async function convertEventToPost(event, authors, subreddit = "") {
-    const convertedData = {
-        kind: "t3",
-        data: {
-            approved_at_utc: null,
-            subreddit: "",
-            selftext: "",
-            author_fullname: "",
-            saved: false,
-            mod_reason_title: null,
-            gilded: 0,
-            clicked: false,
-            title: "",
-            link_flair_richtext: [],
-            subreddit_name_prefixed: "",
-            hidden: false,
-            pwls: 6,
-            link_flair_css_class: null,
-            downs: 0,
-            thumbnail_height: null,
-            top_awarded_type: null,
-            hide_score: false,
-            name: "",
-            quarantine: false,
-            link_flair_text_color: "dark",
-            upvote_ratio: 0,
-            author_flair_background_color: null,
-            subreddit_type: "public",
-            ups: 0,
-            total_awards_received: 0,
-            media_embed: {},
-            media_metadata: {},
-            thumbnail_width: null,
-            author_flair_template_id: null,
-            is_original_content: false,
-            user_reports: [],
-            secure_media: null,
-            is_reddit_media_domain: false,
-            is_meta: false,
-            category: null,
-            secure_media_embed: {},
-            link_flair_text: null,
-            can_mod_post: false,
-            score: 0,
-            approved_by: null,
-            is_created_from_ads_ui: false,
-            author_premium: false,
-            thumbnail: "",
-            edited: false,
-            author_flair_css_class: null,
-            author_flair_richtext: [],
-            gildings: {},
-            content_categories: null,
-            is_self: false,
-            mod_note: null,
-            created: 0,
-            link_flair_type: "text",
-            wls: 6,
-            removed_by_category: null,
-            banned_by: null,
-            author_flair_type: "text",
-            domain: "",
-            allow_live_comments: true,
-            selftext_html: null,
-            likes: null,
-            suggested_sort: null,
-            banned_at_utc: null,
-            view_count: null,
-            archived: false,
-            no_follow: false,
-            is_crosspostable: true,
-            pinned: false,
-            over_18: false,
-            all_awardings: [],
-            awarders: [],
-            media_only: false,
-            can_gild: true,
-            spoiler: false,
-            locked: false,
-            author_flair_text: null,
-            treatment_tags: [],
-            visited: false,
-            removed_by: null,
-            num_reports: null,
-            distinguished: null,
-            subreddit_id: "",
-            author_is_blocked: false,
-            mod_reason_by: null,
-            removal_reason: null,
-            link_flair_background_color: "",
-            id: "",
-            is_robot_indexable: true,
-            report_reasons: null,
-            author: "",
-            discussion_type: null,
-            num_comments: 0,
-            send_replies: true,
-            whitelist_status: "all_ads",
-            contest_mode: false,
-            mod_reports: [],
-            author_patreon_flair: false,
-            author_flair_text_color: null,
-            permalink: "",
-            parent_whitelist_status: "all_ads",
-            stickied: false,
-            url: "",
-            subreddit_subscribers: 0,
-            created_utc: 0,
-            num_crossposts: 0,
-            media: null,
-            is_video: false
-        }
-    };
+    let title = subject(event)
+    const subreddit_name_prefixed = `r/${subreddit}`
+    const finalAuthor = author(authors[event.pubkey], event.pubkey)
+    const author_fullname = `t2_${event.pubkey}`
+    // convertedData.data.name = authors[event.pubkey].display_name
+    // convertedData.data.name = `t3_${eventResponse.id}`;
+    const id = event.id;
+    const created_utc = event.created_at;
+    const permalink = `/r/${subreddit}/comments/${id}`
 
-    convertedData.data.title = subject(event)
 
     // convertedData.data.title = "----------------------------------------------------"
     const contentTrimmed = event.content.trim();
     const match = contentTrimmed.match(onlyUrlRegex);
 
     if (match) {
-        convertedData.data.url = contentTrimmed
-        convertedData.data.domain = match[1]
-
-        try {
-            const { result: metadata } = await ogs({url: event.content})
-            convertedData.data.title ||= metadata.ogTitle
-
-            if (metadata.ogImage[0]) {
-                const preview = {
-                    images: metadata.ogImage.map(image =>
-                        ({
-                            source: {
-                                url: image.url,
-                                width: image.height || 1000,
-                                height: image.width || 1000
-                            },
-                            resolutions: [
-                                {
-                                    url: image.url,
-                                    width: image.height || 1000,
-                                    height: image.width || 1000
-                                }
-                            ],
-                            variants: {},
-                            id: image.url
-                        })
-                    ),
-                    enabled: false
-                }
-
-                convertedData.data.thumbnail = metadata.ogImage[0].url
-                convertedData.data.preview = preview
-            }
-        } catch (e) {}
+        var url = contentTrimmed
+        var domain = match[1]
+        var is_self = false
+        var post_hint = "link"
     } else {
         const processed = await processContent(event)
 
-        convertedData.data.selftext = processed.content
-        convertedData.data.media_metadata = processed.mediaMetadata
-        convertedData.data.is_self = true
+        url = `https://nostr.z.gripe${permalink}`
+        // title ||= processed.content.split(/\r?\n|\r|\n/g)[0] || ''
+        var selftext = processed.content
+        is_self = true
+        var media_metadata = processed.mediaMetadata
     }
 
-    convertedData.data.subreddit = subreddit
-    convertedData.data.author = author(authors[event.pubkey], event.pubkey)
-    convertedData.data.author_fullname = `t2_${event.pubkey}`
-    // convertedData.data.name = authors[event.pubkey].display_name
-    // convertedData.data.name = `t3_${eventResponse.id}`;
-    convertedData.data.id = event.id;
-    convertedData.data.created_utc = event.created_at;
+
+    const convertedData = Everything.post({
+        id,
+        title,
+        url,
+        subreddit,
+        subreddit_name_prefixed,
+        permalink,
+        author: finalAuthor,
+        author_fullname,
+        created_utc,
+        selftext: selftext || '',
+        selftext_html: selftext || '',
+        is_self,
+        domain: domain || '',
+        media_metadata,
+        post_hint: post_hint || ''
+    })
+
+    if (match) {
+        await convertedData.data.buildMetadata()
+    }
+
 
     return convertedData;
 }
--- api/r/[subreddit]/about.js
+++ api/r/[subreddit]/about.js
@@ -1,8 +1,9 @@
-﻿export default async function (request, response) {
-    response.send({
-        kind: "t5",
-        data: {
+﻿import {Everything} from "everything-sdk";
 
-        }
-    })
+export default async function (request, response) {
+    response.send(Everything.group({
+        name: `t5_${request.query.subreddit}`,
+        display_name: `${request.query.subreddit}`,
+        display_name_prefixed: `r/${request.query.subreddit}`
+    }))
 }
